491925f005d666a00567bf7259532b5531ac9d47
Loreto Gabawa Jr <jun.rej@gmail.com>
2017-04-01 22:41:48 +0800

    update

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 	/*
 	 *
-	 * 	Site Name: 		Best Layer, Inc
-	 * 	Description: 	Case study for MSCS534
-	 * 	Author: 			Group 5	 
-	 *	GitHub:				https://github.com/loretog/mscs534
-	 *	Version:			0.1
+	 * 	Framework: 		Lorem Framework
+	 * 	Description: 	Easy to use PHP Framework with small footprint.
+	 * 	Author: 			Loreto Gabawa Jr.
+	 *	GitHub:				https://github.com/loretog/LoremFramework
+	 *	Version:			0.6
 	 *
    */	
 
